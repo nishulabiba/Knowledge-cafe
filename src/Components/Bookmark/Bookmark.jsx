59d52bc3cfa24e8695c5ea21fafb2fca0651b4a0
nishulabiba <nishlabiba2gmail.com>
2023-04-01 02:31:25 +0600

    bookmark updated

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -13,7 +13,13 @@ const Bookmark = (props) => {
             </div>
             <div className="mt-3 bblogs rounded-3">
                 <h5 id='bookm'>Bookmarked Blogs : {b.length}</h5>
-                <p>{b}</p>
+                <div className="mb-2 p-2 ">
+                <p className='para '>{
+                    b.map(c=> 
+                        <p className='mt-2 bg-white p-2 m-2 rounded-3'> {c}</p>
+                        )
+                }</p>
+                </div> 
             </div>
         </div>
     );
